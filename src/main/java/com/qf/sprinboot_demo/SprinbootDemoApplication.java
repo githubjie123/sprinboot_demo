76f768f7274087e64614d7fce1091ba3d65f7e4c
jie <jie@jie.com>
2019-11-28 23:27:46 +0800

    Initial commit

---
+++ src/main/java/com/qf/sprinboot_demo/SprinbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.qf.sprinboot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinbootDemoApplication.class, args);
+    }
+
+}
